0b12fc442613faacbc0cbd6ef52f0ad56b90a6ad
julienbrg <julien@strat.cc>
2025-12-03 14:25:13 +0100

    upgrade

--- src/components/BuildVerification.tsx
+++ src/components/BuildVerification.tsx
@@ -17,7 +17,7 @@ import { brandColors } from '@/theme'
 import Spinner from '@/components/Spinner'
 import { verifyBuildHash, getCurrentBuildHash } from 'w3pk'
 
-const TRUSTED_BUILD_HASH = 'bafybeiaxjbsgqty6qot4tjw7e4y56vgdg5gb3s622hfs6gfr3lfelpjuga' // w3pk v0.8.5
+const TRUSTED_BUILD_HASH = 'bafybeiaehsrukvfhl5b4y2p75iz74ndgel3trjhvwbx5oihlcse5qbiudi' // w3pk v0.8.5
 
 export const BuildVerification = () => {
   const [isVerifying, setIsVerifying] = useState(true)
